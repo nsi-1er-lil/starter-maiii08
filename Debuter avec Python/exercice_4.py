93ba4072f10438c0d5b16cbe4a81b514d06387cd
maiii08 <mailef@lyceechurchill.london>
2024-09-24 15:30:31 +0100

    updat exercice 4

--- Debuter avec Python/exercice_4.py
+++ Debuter avec Python/exercice_4.py
@@ -3,3 +3,14 @@ p=4*c
 a=c**2
 b=(a>5)
 print("a= "+str(a)+ "b= "+str(b)+"p= "+str(p))
+
+def perimetre(x) :
+    return 4*x
+
+print(perimetre(c))
+
+def surface(x) :
+    return x**2
+
+print(surface(c))
+
